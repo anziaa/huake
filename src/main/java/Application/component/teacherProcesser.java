db4603013548966b52cb2b5a8015e5b167ceab16
anziaa <2375259233@qq.com>
2022-10-18 19:21:42 +0800

    Initial commit

---
+++ src/main/java/Application/component/teacherProcesser.java
@@ -0,0 +1,44 @@
+package Application.component;
+
+import Application.entity.TeaInfo;
+import org.springframework.stereotype.Component;
+import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.processor.PageProcessor;
+import us.codecraft.webmagic.selector.Html;
+import us.codecraft.webmagic.selector.Selectable;
+import us.codecraft.webmagic.selector.Selector;
+
+import java.util.List;
+
+@Component
+public class teacherProcesser implements PageProcessor {
+
+    @Override
+    public void process(Page page) {
+        Html html =page.getHtml();
+        List<String> links =html.css("div.js_menu").links().all();
+        page.addTargetRequests(links);
+        page.getResultItems().setSkip(true);
+        if(links.size() ==0){
+        parseTeaInfo(page);
+        }
+
+
+    }
+
+    private void parseTeaInfo(Page page) {
+        Html html = page.getHtml();
+        String name = html.css("div.info h2","text").get();
+        System.out.println(name);
+        List<Selectable> Info = html.css("div.blockwhite.Psl-info>div.cont p","text").nodes();
+        System.out.println(Info);
+
+    }
+
+
+    @Override
+    public Site getSite() {
+        return Site.me();
+    }
+}
